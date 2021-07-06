f05e425761f2cb3d84174e655043389d6ebb9631
nickkauf <nkauf87@gmail.com>
2021-07-06 16:15:49 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,9 @@
+/*
+This is the first Java program which will print out a message. Hello world.
+ */
+public class MyFirstJava{
+    public static void main (String[] args)
+    {
+        System.out.println("My name is Nick");
+    }
+}
